b81a7bc3863edc64f5e26a3b07f749cc449b7dbd
Saints <lukas-padilha@hotmail.com>
2020-02-04 17:06:29 -0300

    minor updates

--- RoomControl/app/src/main/java/com/wise/roomcontrol/LoginActivity.java
+++ RoomControl/app/src/main/java/com/wise/roomcontrol/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.wise.roomcontrol;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,18 +16,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.wise.roomcontrol.classes.User;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText campoLogin;
     private EditText campoSenha;
     final private Dao dao=new Dao();
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        prefs=getApplicationContext().getSharedPreferences("Descartes",MODE_PRIVATE);
+        editor=prefs.edit();
         setContentView(R.layout.activity_login);
         campoLogin=findViewById(R.id.email);
         campoSenha=findViewById(R.id.password);
+        final CheckBox caixa=findViewById(R.id.checkBox);
+        try{
+            if(prefs.getBoolean("lembrarme",false)) {
+                campoLogin.setText(prefs.getString("email", null));
+                campoSenha.setText(prefs.getString("senha", null));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         final Button botaologin = findViewById(R.id.login);
         botaologin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,11 +56,30 @@ public class LoginActivity extends AppCompatActivity {
                     String password = campoSenha.getText().toString();
                     Log.i("teste", "login: "+login+"    senha: "+password);
                     if(dao.validaLogin(login, password)) {
-                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        try{
+                            String[] a1={"email"};
+                            String[] a2={campoLogin.getText().toString()};
+                            JSONObject obj = new JSONObject(dao.ServerInOutput(false, "usuario/getByEmail", a1, a2));
+                            int id=obj.getInt("id");
+                            String nome = obj.getString("nome");
+                            System.out.println(nome);
+                            int emp = obj.getJSONObject("idOrganizacao").getInt("id");
+                            System.out.println(emp);
+                            dao.logado = new User(campoLogin.getText().toString(),campoSenha.getText().toString(),nome,emp,id);
+                            editor.putString("email",campoLogin.getText().toString());
+                            editor.putString("senha",campoSenha.getText().toString());
+                            if(caixa.isChecked()){
+                                editor.putBoolean("lembrarme",true);
+                            }
+                            editor.commit();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
                     }else
                         Toast.makeText(LoginActivity.this, R.string.login_failed, Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(LoginActivity.this, R.string.login_failed, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, R.string.login_failed+". Preencha corretamente os campos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -54,8 +94,18 @@ public class LoginActivity extends AppCompatActivity {
         skipper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                dao.logado=null;
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(dao.logado!=null){
+            Toast.makeText(LoginActivity.this,"Sessão finalizada. Favor logar novamente",Toast.LENGTH_LONG);
+            dao.logado=null;
+        }
     }
 }
